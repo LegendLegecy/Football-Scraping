2bb9167afeec53809bdabe38496ed0c039f92900
Legends Legecy <legendslegecy1@gmail.com>
2025-06-27 13:22:32 +0500

    initial commit

--- Main.py
+++ Main.py
@@ -5,7 +5,10 @@ import time
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 import re
-
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def delete_html_files():
     """Delete all .html files in the current working directory."""
@@ -41,10 +44,13 @@ def fetch_and_save_html(url: str, filename: str,code:bool=False,pretty:bool=True
         try:
             driver.get(url)
 
-            for _ in range(10):
+            for _ in range(5):
                 driver.refresh()
-                time.sleep(2)
-            time.sleep(30)
+                time.sleep(1)
+            
+            WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "div.flex.flex-col"))
+            )
             
             html = driver.page_source
             if pretty==True:
@@ -64,6 +70,7 @@ def fetch_and_save_html(url: str, filename: str,code:bool=False,pretty:bool=True
             print(f"\n Got The html code from : {url}")
     except Exception as e:
         print(f"Error: {e}")
+    return 'success'
 
 def get_href(url:str,filename:str):
     hrefs=[]
@@ -189,6 +196,7 @@ def write_odds_to_excel(odds, columns, row, excel_path,purpose:str):
 
 
     wb.save(excel_path)
+    print(odds[bookmaker])
     print(f"✅ Odds written to {excel_path}")
 
 def extract_odds_by_bookmaker(filename, row_selector, odds_count, odds_labels):
@@ -256,8 +264,11 @@ def get_yes_no_odds_by_bookmaker(filename):
 
 if __name__ == "__main__":
     url = input("\nEnter the url\nHINT: https://www.oddsportal.com/football/LEAGUE-COUNTRY/LEAGUE-NAME/standings\n-2024-2025 after league-name if needed\n : ")
+    # url = "https://www.oddsportal.com/football/spain/laliga/standings"
     league_country = url.split("/football/")[-1].split("/")[0]
     league_name = url.split(f"{league_country}/")[-1].split("/")[0]
+    output_file = str(input("\nEnter Output Excel File Name:"))
+    output_file = output_file if output_file.endswith('.xlsx') else output_file + '.xlsx'
     print(f"League Country: {league_country}")
     print(f"League Name: {league_name}")
 
@@ -265,125 +276,137 @@ if __name__ == "__main__":
     
 
     links=get_href(url.split("/standing")[0], f"{league_country}-{league_name}.html")
-
-
+    links = list(set(links))
     counter=0
     for link in links:
         counter+=1
         row= counter+8
-        
-        print(f"\n{link}")
+        if row > 13:
+            try:
+                print(f"\n{link}")
 
-        # Sanitize filename to avoid leading slashes and invalid characters
-        raw_filename = link.split(league_name)[-1]
-        sanitized_filename = raw_filename.lstrip('/').replace('/', '_').replace('\\', '_') + ".html"
-        output_file = f"{league_country}-{league_name}.xlsx"
+                # Sanitize filename to avoid leading slashes and invalid characters
+                raw_filename = link.split(league_name)[-1]
+                sanitized_filename = raw_filename.lstrip('/').replace('/', '_').replace('\\', '_') + ".html"
+                # output_file = f"{league_country}-{league_name}.xlsx"
+                
 
-        fetch_and_save_html(link, sanitized_filename,True)
+                fetch_and_save_html(link, sanitized_filename,True)
 
-        get_date(sanitized_filename, str(row), output_file)
+                get_date(sanitized_filename, str(row), output_file)
 
-        get_teams(sanitized_filename, str(row), output_file)
+                get_teams(sanitized_filename, str(row), output_file)
 
-        get_results(sanitized_filename, str(row), output_file)
+                get_results(sanitized_filename, str(row), output_file)
+                
+                print("\nWaiting to fetch odds for FULL TIME 1x2")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#1X2;2", sanitized_filename, True,  )
+                    if fetch == 'success':
+                        odds = get_1x2_odds_by_bookmaker(sanitized_filename)
+                        # Check if any value in odds dict is empty
+                        columns = ['M','N','O','P','Q','R','S','T','U']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            write_odds_to_excel(odds, columns, row, output_file,'1x2')
+                            break
+                    
+                
+                print("\nWaiting to fetch odds for 1st HALF 1x2")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#1X2;3", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_1x2_odds_by_bookmaker(sanitized_filename)
+                        columns = ['W','X','Y','Z','AA','AB','AC','AD','AE']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            write_odds_to_excel(odds, columns, row, output_file,'1x2')
+                            break
 
-        print("\nWaiting to fetch odds for FULL TIME 1x2")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#1X2;2", sanitized_filename, True,  )
-            odds = get_1x2_odds_by_bookmaker(sanitized_filename)
-            # Check if any value in odds dict is empty
-            columns = ['M','N','O','P','Q','R','S','T','U']
-            if any(vv for v in odds.values() for vv in v.values()):
-                write_odds_to_excel(odds, columns, row, output_file,'1x2')
-                break
+
+
+                # OVER/UNDER FULL TIME
+                print("\nWaiting to fetch odds for FULL TIME 1.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;2;1.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['AG','AH','AI','AJ','AK','AL']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
+
+                print("\nWaiting to fetch odds for FULL TIME 2.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;2;2.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['AN','AO','AP','AQ','AR','AS']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
+
+                print("\nWaiting to fetch odds for FULL TIME 3.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;2;3.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['AU','AV','AW','AX','AY','AZ']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
+
+                
+
+                # OVER/UNDER 1ST HALF
+                print("\nWaiting to fetch odds for 1ST HALF 0.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;3;0.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['BB','BC','BD','BE','BF','BG']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
+
+                print("\nWaiting to fetch odds for 1ST HALF 1.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;3;1.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['BI','BJ','BK','BL','BM','BN']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
+
+                print("\nWaiting to fetch odds for 1ST HALF 2.5 over/under")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#over-under;3;2.50;0", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_over_under_odds_by_bookmaker(sanitized_filename)
+                        columns = ['BP','BQ','BR','BS','BT','BU']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            
+                            write_odds_to_excel(odds, columns, row, output_file,'over/under')
+                            break
             
-        
-        print("\nWaiting to fetch odds for 1st HALF 1x2")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#1X2;3", sanitized_filename, True, )
-            odds = get_1x2_odds_by_bookmaker(sanitized_filename)
-            columns = ['W','X','Y','Z','AA','AB','AC','AD','AE']
-            if any(vv for v in odds.values() for vv in v.values()):
-                write_odds_to_excel(odds, columns, row, output_file,'1x2')
-                break
 
 
+                print("\nWaiting to fetch odds for BOTH TEAMS TO SCORE")
+                for _ in range(2):
+                    fetch = fetch_and_save_html(link+"#bts;2", sanitized_filename, True, )
+                    if fetch == 'success':
+                        odds = get_yes_no_odds_by_bookmaker(sanitized_filename)
+                        columns = ['BW','BX','BY','BZ','CA','CB']
+                        if any(vv for v in odds.values() for vv in v.values()):
+                            write_odds_to_excel(odds, columns, row, output_file,'yes/no')
+                            break
+            except:
+                pass
 
-        # OVER/UNDER FULL TIME
-        print("\nWaiting to fetch odds for FULL TIME 1.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;2;1.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['AQ','AR','AS','AT','AU','AV']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
+            # delete_html_files()
 
-        print("\nWaiting to fetch odds for FULL TIME 2.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;2;2.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['AX','AY','AZ','BA','BB','BC']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
-
-        print("\nWaiting to fetch odds for FULL TIME 3.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;2;3.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['BE','BF','BG','BH','BI','BJ']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
-
-        
-
-        # OVER/UNDER 1ST HALF
-        print("\nWaiting to fetch odds for 1ST HALF 0.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;3;0.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['BL','BM','BN','BO','BP','BQ']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
-
-        print("\nWaiting to fetch odds for 1ST HALF 1.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;3;1.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['BS','BT','BU','BV','BW','BX']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
-
-        print("\nWaiting to fetch odds for 1ST HALF 2.5 over/under")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#over-under;3;2.50;0", sanitized_filename, True, )
-            odds = get_over_under_odds_by_bookmaker(sanitized_filename)
-            columns = ['BZ','CA','CB','CC','CD','CE']
-            if any(vv for v in odds.values() for vv in v.values()):
-                 
-                write_odds_to_excel(odds, columns, row, output_file,'over/under')
-                break
-    
-
-
-        print("\nWaiting to fetch odds for BOTH TEAMS TO SCORE")
-        for _ in range(2):
-            fetch_and_save_html(link+"/#bts;2", sanitized_filename, True, )
-            odds = get_yes_no_odds_by_bookmaker(sanitized_filename)
-            columns = ['DB','DC','DD','DE','DF','DG']
-            if any(vv for v in odds.values() for vv in v.values()):
-                write_odds_to_excel(odds, columns, row, output_file,'yes/no')
-                break
-        if row ==9:
-            break
-
-        delete_html_files()
+input("\n\n\nPress Enter to Exit . . . ")
